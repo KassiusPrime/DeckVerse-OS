e376542ce846773c8fe88394dd1ec886cc206015
KassiusPrime <cassianokaique9@gmail.com>
2026-09-03 14:37:26 -0300

    fix: avoid recursive regex replacement in Drive resync patch

--- api/admin/resync-drive-patched.js
+++ api/admin/resync-drive-patched.js
@@ -6,7 +6,8 @@ export default async function patchedHandler(req, res) {
   const originalExec = RegExp.prototype.exec;
   RegExp.prototype.exec = function patchedExec(input) {
     if (this.source.includes('\\z')) {
-      const fixed = new RegExp(this.source.replace(/\\z/g, '(?![\\s\\S])'), this.flags);
+      const fixedSource = this.source.split('\\z').join('(?![\\s\\S])');
+      const fixed = new RegExp(fixedSource, this.flags);
       fixed.lastIndex = this.lastIndex;
       const result = originalExec.call(fixed, input);
       this.lastIndex = fixed.lastIndex;
