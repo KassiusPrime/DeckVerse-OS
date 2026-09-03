feb1b0f8d701eaee1e13d28d4eef31ae5cdd1f89
KassiusPrime <cassianokaique9@gmail.com>
2026-09-03 14:36:30 -0300

    fix: patch Drive master end-of-file regex for resync

---
+++ api/admin/resync-drive-patched.js
@@ -0,0 +1,22 @@
+import handler from './resync-drive-current.js';
+
+export const config = { maxDuration: 60 };
+
+export default async function patchedHandler(req, res) {
+  const originalExec = RegExp.prototype.exec;
+  RegExp.prototype.exec = function patchedExec(input) {
+    if (this.source.includes('\\z')) {
+      const fixed = new RegExp(this.source.replace(/\\z/g, '(?![\\s\\S])'), this.flags);
+      fixed.lastIndex = this.lastIndex;
+      const result = originalExec.call(fixed, input);
+      this.lastIndex = fixed.lastIndex;
+      return result;
+    }
+    return originalExec.call(this, input);
+  };
+  try {
+    return await handler(req, res);
+  } finally {
+    RegExp.prototype.exec = originalExec;
+  }
+}
