0070aa192959857b8f79f4a524af648f1fb36566
KassiusPrime <cassianokaique9@gmail.com>
2026-09-04 00:26:30 -0300

    chore: add protected preview import probe

---
+++ api/admin/import-drive-preview.js
@@ -0,0 +1,5 @@
+export const config = { maxDuration: 300 };
+export default async function handler(req, res) {
+  if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
+  return res.status(200).json({ ok: true, protectedPreview: true });
+}
