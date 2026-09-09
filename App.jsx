3f39d0f3b44dcef159d7387fb2e8ff64ba98bded
KassiusPrime <cassianokaique9@gmail.com>
2026-09-09 11:52:30 -0300

    feat: route admin content center with forms support

--- App.jsx
+++ App.jsx
@@ -23,7 +23,7 @@ import GameHub from './GameHub';
 import AdminSupabase from './AdminSupabase';
 import AdminSynopsis from './AdminSynopsis';
 import AdminCardValues from './AdminCardValues';
-import AdminContentManager from './AdminContentManagerV2';
+import AdminContentManager from './AdminContentCenter';
 
 function AdminRouteGuard({ children }) {
   const { isAuthenticated, isAdmin, isLoadingAuth } = useAuth();
