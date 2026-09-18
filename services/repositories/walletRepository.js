9da707da1761ec50f7586e0ec0fb6dd7c70e9458
KassiusPrime <cassianokaique9@gmail.com>
2026-09-18 08:41:08 -0300

    Add transaction IDs to services/repositories/walletRepository.js

--- services/repositories/walletRepository.js
+++ services/repositories/walletRepository.js
@@ -4,8 +4,8 @@ const supabase=()=>getSupabaseBrowserClient();
 const rpc=(name,args)=>supabase().rpc(name,args).then(({data,error})=>{if(error) throw error; return data;});
 
 export const walletRepository=Object.freeze({
- grant:(playerId,amount,type,referenceType=null,referenceId=null,reason='Currency grant')=>rpc('grant_currency',{p_player_id:playerId,p_amount:amount,p_transaction_type:type,p_reference_type:referenceType,p_reference_id:referenceId,p_reason:reason}),
- remove:(playerId,amount,type,referenceType=null,referenceId=null,reason='Currency removal')=>rpc('remove_currency',{p_player_id:playerId,p_amount:amount,p_transaction_type:type,p_reference_type:referenceType,p_reference_id:referenceId,p_reason:reason}),
- transfer:(from,to,amount,referenceType=null,referenceId=null,reason='Currency transfer')=>rpc('transfer_currency',{p_from:from,p_to:to,p_amount:amount,p_reference_type:referenceType,p_reference_id:referenceId,p_reason:reason}),
+ grant:(playerId,amount,type,referenceType=null,referenceId=null,reason='Currency grant',transactionId)=>rpc('grant_currency_with_transaction',{p_player_id:playerId,p_amount:amount,p_transaction_type:type,p_reference_type:referenceType,p_reference_id:referenceId,p_reason:reason,p_transaction_id:transactionId}),
+ remove:(playerId,amount,type,referenceType=null,referenceId=null,reason='Currency removal',transactionId)=>rpc('remove_currency_with_transaction',{p_player_id:playerId,p_amount:amount,p_transaction_type:type,p_reference_type:referenceType,p_reference_id:referenceId,p_reason:reason,p_transaction_id:transactionId}),
+ transfer:(from,to,amount,referenceType=null,referenceId=null,reason='Currency transfer',transactionId)=>rpc('transfer_currency_with_transaction',{p_from:from,p_to:to,p_amount:amount,p_reference_type:referenceType,p_reference_id:referenceId,p_reason:reason,p_transaction_id:transactionId}),
 });
 export default walletRepository;
