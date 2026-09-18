ecb816d894433495b0d1d53b19c3d88aa9ebdec1
KassiusPrime <cassianokaique9@gmail.com>
2026-09-18 08:44:38 -0300

    Integrate Event Bus into services/marketplace/marketplaceService.js

--- services/marketplace/marketplaceService.js
+++ services/marketplace/marketplaceService.js
@@ -2,7 +2,8 @@ import { marketplaceRepository } from '../repositories/marketplaceRepository.js'
 import { createPurchaseMarketListingUseCase } from '../useCases/purchaseMarketListing.js';
 import { createCreateMarketListingUseCase } from '../useCases/createMarketListing.js';
 import { createCancelMarketListingUseCase } from '../useCases/cancelMarketListing.js';
-
+import { eventBus } from '../../events/EventBus.js';
+import { CardPurchasedEvent } from '../../events/CardPurchasedEvent.js';
 const purchase=createPurchaseMarketListingUseCase(marketplaceRepository);
 const create=createCreateMarketListingUseCase(marketplaceRepository);
 const cancel=createCancelMarketListingUseCase(marketplaceRepository);
@@ -10,7 +11,7 @@ export const marketplaceService=Object.freeze({
   getCurrentProfile: marketplaceRepository.getCurrentProfile,
   browse: marketplaceRepository.browse,
   getOwnedCards: marketplaceRepository.getOwnedCards,
-  purchase:(listingId)=>purchase.execute(listingId),
+  purchase:async(listingId,transactionId)=>{const result=await purchase.execute(listingId,transactionId);await eventBus.emit(CardPurchasedEvent(result));return result;},
   createListing:(cardId,quantity,priceDc)=>create.execute(cardId,quantity,priceDc),
   cancelListing:(listingId)=>cancel.execute(listingId),
 });
