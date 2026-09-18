4c484b57a6c2ad5d5c9a99236635e844ee183706
KassiusPrime <cassianokaique9@gmail.com>
2026-09-18 08:44:41 -0300

    Integrate Event Bus into services/inventory/inventoryService.js

--- services/inventory/inventoryService.js
+++ services/inventory/inventoryService.js
@@ -1,30 +1,12 @@
 import inventoryRepository from '../repositories/inventoryRepository.js';
-import {
-  createGrantCardUseCase,
-  createRemoveCardUseCase,
-  createTransferCardUseCase,
-  createEquipItemUseCase,
-  createUnequipItemUseCase,
-} from '../useCases/inventoryUseCases.js';
-
-const grant = createGrantCardUseCase(inventoryRepository);
-const remove = createRemoveCardUseCase(inventoryRepository);
-const transfer = createTransferCardUseCase(inventoryRepository);
-const equip = createEquipItemUseCase(inventoryRepository);
-const unequip = createUnequipItemUseCase(inventoryRepository);
-
-export const inventoryService = Object.freeze({
-  getMyRosterWithArtwork: inventoryRepository.getMyRosterWithArtwork,
-  getMyCardArtwork: inventoryRepository.getMyCardArtwork,
-  getMyCardOwnership: inventoryRepository.getMyCardOwnership,
-  savePlayerCardArtwork: inventoryRepository.savePlayerCardArtwork,
-  clearPlayerCardArtwork: inventoryRepository.clearPlayerCardArtwork,
-  grantCard: (profileId, cardId, copies, reason) => grant.execute(profileId, cardId, copies, reason),
-  removeCard: (profileId, cardId, copies, reason) => remove.execute(profileId, cardId, copies, reason),
-  transferCard: (fromProfileId, toProfileId, cardId, copies, reason) =>
-    transfer.execute(fromProfileId, toProfileId, cardId, copies, reason),
-  equipItem: (characterCardId, slot, itemCardId) => equip.execute(characterCardId, slot, itemCardId),
-  unequipItem: (characterCardId, slot) => unequip.execute(characterCardId, slot),
+import {createGrantCardUseCase,createRemoveCardUseCase,createTransferCardUseCase,createEquipItemUseCase,createUnequipItemUseCase} from '../useCases/inventoryUseCases.js';
+import {eventBus} from '../../events/EventBus.js';
+import {InventoryTransferredEvent} from '../../events/InventoryTransferredEvent.js';
+const grant=createGrantCardUseCase(inventoryRepository), remove=createRemoveCardUseCase(inventoryRepository), transfer=createTransferCardUseCase(inventoryRepository), equip=createEquipItemUseCase(inventoryRepository), unequip=createUnequipItemUseCase(inventoryRepository);
+export const inventoryService=Object.freeze({
+ getMyRosterWithArtwork:inventoryRepository.getMyRosterWithArtwork,getMyCardArtwork:inventoryRepository.getMyCardArtwork,getMyCardOwnership:inventoryRepository.getMyCardOwnership,savePlayerCardArtwork:inventoryRepository.savePlayerCardArtwork,clearPlayerCardArtwork:inventoryRepository.clearPlayerCardArtwork,
+ grantCard:(...a)=>grant.execute(...a),removeCard:(...a)=>remove.execute(...a),
+ transferCard:async(...a)=>{const result=await transfer.execute(...a);await eventBus.emit(InventoryTransferredEvent(result));return result;},
+ equipItem:(...a)=>equip.execute(...a),unequipItem:(...a)=>unequip.execute(...a),
 });
-
 export default inventoryService;
