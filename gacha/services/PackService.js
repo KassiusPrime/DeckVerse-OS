26a3027cb289e9456c38e35950044c59295f1216
KassiusPrime <cassianokaique9@gmail.com>
2026-09-22 00:38:53 -0300

    feat(gacha): attach pack audit summary to event

--- gacha/services/PackService.js
+++ gacha/services/PackService.js
@@ -1,11 +1,12 @@
 import OpenPackUseCase from '../useCases/OpenPackUseCase.js';
 import { eventBus } from '../../events/EventBus.js';
 import { PackOpenedEvent } from '../events/PackOpenedEvent.js';
+import PackAuditService from './PackAuditService.js';
 
 export const PackService = Object.freeze({
   async openPack(count = 1) {
     const result = await OpenPackUseCase.execute({ count });
-    await eventBus.emit(PackOpenedEvent(result));
+    await eventBus.emit(PackOpenedEvent({ ...result, audit: PackAuditService.summarize(result) }));
     return result;
   },
 });
